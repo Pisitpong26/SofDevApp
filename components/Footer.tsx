ea3e711590123f9c37a9f4c64a094d3823e91466
Xcaliburth <88648253+Xcaliburth@users.noreply.github.com>
2023-09-19 16:44:14 +0700

    AttractionPAge and asset

---
+++ components/Footer.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+
+class Footer extends Component {
+  render() {
+    const current_year = new Date().getFullYear();
+    return (
+        
+        <div style={{ textAlign: 'center' }}><h1 style={{
+            color: '#285C79',
+            textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+            fontFamily: 'Courgette',
+            fontSize: '150px',
+            fontStyle: 'normal',
+            fontWeight: 400,
+            lineHeight: 'normal'
+        }}>TravelerTale</h1>
+        <img
+                src="../Footer/footer-removebg-preview 2.png"
+                alt="Footer"
+                className="w-full" /></div>
+    );
+  }
+}
+
+export default Footer;
