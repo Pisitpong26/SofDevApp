029d957f731fb5c4ae34d67ffd3237f5a56ec0d1
16sakuraa <64010860@kmitl.ac.th>
2023-09-26 00:01:58 +0700

    minor bug fixes

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -15,7 +15,7 @@ export default function About(){
                 <label className="text-sm font-medium text-gray-900 sr-only dark:text-white ">Search</label>
                 <div className="relative">
                     <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm border-2 border-gray-300 rounded-3xl bg-gray-50 focus:ring-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 " placeholder="Search Attraction..." required></input>
-                    <button type="submit" className="text-black absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Lets Go!</button>
+                    <button type="submit" className="text-black absolute right-10 border-2 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Lets Go!</button>
                 </div>
             </form>
         </div>
