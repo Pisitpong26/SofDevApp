a1e3902f646624728bb1d2d61c6955fb35c539db
16sakuraa <64010860@kmitl.ac.th>
2023-09-25 23:02:10 +0700

    Add gap between attract card

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -12,16 +12,38 @@ export default function About(){
             <h1 className='text-6xl md:text-6xl uppercase font-bold mt-3 text-blue tracking-widest'>แหล่งท่องเที่ยวธรรมชาติ</h1>
 
             <form className="mt-10 mb-10 w-full max-w-3xl">
-                <label className="text-sm font-medium text-gray-900 sr-only dark:text-white border-2">Search</label>
+                <label className="text-sm font-medium text-gray-900 sr-only dark:text-white ">Search</label>
                 <div className="relative">
-                    <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm border-2 border-gray-300 rounded-3xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Attraction..." required></input>
+                    <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm border-2 border-gray-300 rounded-3xl bg-gray-50 focus:ring-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 " placeholder="Search Attraction..." required></input>
                     <button type="submit" className="text-black absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Lets Go!</button>
                 </div>
             </form>
         </div>
 
 
-        <div className='max-w-screen-2xl flex flex-row justify-center items-center'>
+        <div className='max-w-screen-2xl mx-auto flex flex-row justify-center items-center gap-8' >
+            <div>
+                <AttractCard imgSrc="../Detail/waterfall.png" imgAlt="placeholder" text="น้ำตกทีลอซู"> </AttractCard>
+            </div>
+            <div>
+                <AttractCard imgSrc="../Detail/bluecave.png" imgAlt="placeholder" text="ถ้ำสีฟ้า"> </AttractCard>
+            </div>
+            <div>
+                <AttractCard imgSrc="../Detail/somewhere.png" imgAlt="placeholder" text="ดอยมะม่วงสามหมื่น"> </AttractCard>
+            </div>
+        </div>
+        <div className='max-w-screen-2xl mx-auto flex flex-row justify-center items-center gap-8' >
+            <div>
+                <AttractCard imgSrc="../Detail/Flower.png" imgAlt="placeholder" text="ม่อนหมอกตะวัน"> </AttractCard>
+            </div>
+            <div>
+                <AttractCard imgSrc="../Detail/waterfall2.png" imgAlt="placeholder" text="น้ำตกลานสาง"> </AttractCard>
+            </div>
+            <div>
+                <AttractCard imgSrc="../Detail/mountain.png" imgAlt="placeholder" text="ม่อนคลุยหลวง ดอยทูเล"> </AttractCard>
+            </div>
+        </div>
+        <div className='max-w-screen-2xl mx-auto flex flex-row justify-center items-center gap-8' >
             <div>
                 <AttractCard imgSrc="../Detail/waterfall.png" imgAlt="placeholder" text="น้ำตก"> </AttractCard>
             </div>
--- components/AttractCard.tsx
+++ components/AttractCard.tsx
@@ -9,13 +9,15 @@ interface AttractCard {
 
 export const AttractCard: React.FC<AttractCard> = ({ imgSrc, imgAlt, text }) => {
   return (
-    <div className='mt-4 flex-col relative hover:scale-110 '>
-      <img src={imgSrc} alt={imgAlt} className="max-w-md h-auto mb-4 rounded-t-2xl " />
+    <div className='mt-4 flex-col relative hover:scale-110 shadow-2xl border-spacing-4'>
+      <img src={imgSrc} alt={imgAlt} className="w-[416px] h-[259px] mb-4 rounded-t-2xl " />
       <div className="absolute w-full h-16 bottom-0 bg-black bg-opacity-50">
-        <h3 className="absolute text-2xl text-yellow-300 bottom-3 left-5 font-bold"> {text}</h3>
+        <h3 className="absolute text-xl text-yellow-300 bottom-5 left-5 font-bold"> {text}</h3>
       </div>
     </div>
   );
 };
 
 export default AttractCard;
+
+// <img src={imgSrc} alt={imgAlt} className="max-w-md h-auto mb-4 rounded-t-2xl " />
