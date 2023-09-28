51702f567e208dd93083a0d6f1fb1240857fd9be
16sakuraa <64010860@kmitl.ac.th>
2023-09-28 15:20:58 +0700

    Cleanup Login Page

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -8,23 +8,6 @@ export default function Login(){
                 <div className="flex flex-col justify-center items-center " >
                     <div className="mt-20 flex flex-col items-center border-2 rounded-2xl backdrop-blur-sm w-[430px] h-[400px]">
                         <div className=" text-white text-3xl mt-10 font-bold ">Login</div>
-                        {/* <form>
-                            <div className="mt-10 relative w-[350px] h-[100px] items-center justify-center gap-6 " >
-                            
-                                    <input
-                                    type="text"
-                                    className="h-[45px] w-full rounded bg-white bg-opacity-20 text-white  border-b border-white placeholder-white text-left" 
-                                    id="exampleFormControlInput2"
-                                    placeholder="Username" />
-
-                                    <input
-                                    type="text"
-                                    className="mt-5 h-[45px] w-full rounded bg-white bg-opacity-20 text-white  border-b border-white placeholder-white text-left" 
-                                    id="exampleFormControlInput2"
-                                    placeholder="Password" />
-
-                            </div>
-                        </form> */}
                         <div className="w-full max-w-xs">
                             <form className="mt-4 rounded px-3 pt-5 pb-8 mb-4">
                                 <div className="mb-4">
@@ -50,7 +33,7 @@ export default function Login(){
                                 </div>
                             </form>
                                     <p className="text-center text-white text-xs">
-                                        TravelerTale Developer Team, made with ❤️
+                                    Made with ❤️ by TravelerTale Developer Team
                                     </p>
                             </div>
                             
@@ -61,5 +44,3 @@ export default function Login(){
         </main>    
     )
 }
-
-//w-full h-50 bg-black bg-opacity-40 text-black
